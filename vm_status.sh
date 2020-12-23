5db99ddf32a2643b20709cbc9c3cae8294b633b4
Scott Finnie <scott.finnie@hymans.co.uk>
2020-12-23 18:37:30 +0000

    show VM IP in vm_status script

--- vm_status.sh
+++ vm_status.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 az vm get-instance-view --resource-group fastai2 --name fastai2 -o table
+IP=$(az vm show -d -g fastai2 -n fastai2 --query publicIps -o tsv)
+echo "Jupyter notebook available at https://${IP}:8000/"

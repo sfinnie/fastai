9ae15f3a818a056486be1459cdcab3b84639e7a5
Scott Finnie <scott.finnie@hymans.co.uk>
2020-12-23 17:27:32 +0000

    add utility scripts for starting & stopping VM

--- setup/start_vm.sh
+++ setup/start_vm.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-echo "Starting VM..>"
+echo "Starting VM..."
 az vm start --resource-group fastai2 --name fastai2
-az vm list-ip-addresses --resource-group fastai2 --name fastai2 -o table
+IP=$(az vm show -d -g fastai2 -n fastai2 --query publicIps -o tsv)
+echo "VM started, Jupyter notebook available at https://${IP}:8000/"
+echo "Login with username fastuser"
+git web--browse https://${IP}:8000/
+# az vm list-ip-addresses --resource-group fastai2 --name fastai2 -o table
--- setup/stop_vm.sh
+++ setup/stop_vm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Stopping VM..>"
+echo "Stopping VM..."
 az vm deallocate --resource-group fastai2 --name fastai2
 ./vm_status.sh
